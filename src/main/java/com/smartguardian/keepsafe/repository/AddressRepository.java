594334fb0d5f023dc75c7f36aae64e4a011adfb0
Arthur D’Afonseca <arthur.dafonseca89@gmail.com>
2022-04-20 19:19:31 -0300

    Add custom requests to AddressRepository and PhoneRepository

--- src/main/java/com/smartguardian/keepsafe/repository/AddressRepository.java
+++ src/main/java/com/smartguardian/keepsafe/repository/AddressRepository.java
@@ -1,7 +1,9 @@
 package com.smartguardian.keepsafe.repository;
 
+import java.util.List;
 import com.smartguardian.keepsafe.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<Address, Integer> {
+  List<Address> findByUserId(int userId);
 }
